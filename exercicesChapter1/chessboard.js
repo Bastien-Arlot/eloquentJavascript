252622e84581986b5e01355ded22d1b12a3a7ff4
Bastien Arlot <arlot.b@gmail.com>
2022-04-15 16:13:43 +0200

    add solution from book and fixed mine cause i printed everything into the console at each line instead of stocking into a variable and printing the variable at the end

--- exercicesChapter1/chessboard.js
+++ exercicesChapter1/chessboard.js
@@ -1,5 +1,6 @@
 // First tentative but wasnt satisfied of the non modulable code + didnt check that lines were not all the same
 let size = 8;
+let board = "";
 console.log(("#" + " " + "#" + " " + "#" + " " + "#" + "\n").repeat(size));
 
 // Solution that answer the problem
@@ -7,9 +8,27 @@ let a = " ";
 let b = "#"
 for(let i = 1; i <= size; i++){
    if (i % 2 !== 0){
-      console.log(`${(a+b).repeat(4)} `);
+      board += (a+b).repeat(4);
+      board += "\n";
    }else{
-      console.log(`${(b+a).repeat(4)} `);
+      board += (b+a).repeat(4);
+      board += "\n";
    }
 }
+console.log(board)
+// Solution from the book using double loop and stocking the result and didnt do it myself
 
+
+
+for (let y = 0; y < size; y++) {
+   for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+         board += " ";
+      } else {
+         board += "#";
+      }
+   }
+   board += "\n";
+}
+
+console.log(board);
