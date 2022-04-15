4e1e4015672a46e0465cf2a100a96a2d4bcb18af
Bastien Arlot <arlot.b@gmail.com>
2022-04-15 15:48:56 +0200

    add ideal solution

--- exercicesChapter1/fizzBuzz.js
+++ exercicesChapter1/fizzBuzz.js
@@ -1,4 +1,4 @@
-// My first solution
+// // My first solution
 for(let i = 0; i <= 100; i++){
     if (i % 5 === 0 && i % 3 === 0) {
         console.log("FizzBuzz");
@@ -9,4 +9,11 @@ for(let i = 0; i <= 100; i++){
     }else{
         console.log(i);
     }
+}
+// Best solution from the book
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
 }
