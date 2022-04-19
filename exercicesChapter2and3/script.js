614576b547fb7c702e1bf687490dea747e500500
Bastien Arlot <arlot.b@gmail.com>
2022-04-19 15:57:29 +0200

    add the solution from the book

--- exercicesChapter2and3/script.js
+++ exercicesChapter2and3/script.js
@@ -35,3 +35,29 @@ const countChar = (string, search) => {
     return string.split("").filter(x => x === search).length;
 }
 console.log(countChar("Bonjour les CooPains Anglais aah", "a"));
+
+const countBs2 = (string) => {
+    return countChar(string, "B");
+}
+console.log(countBs("Bonjour les amis BBBBBb"));
+
+// Add of the solution without using map from the book, not a fan cause less readable.
+
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == ch) {
+            counted += 1;
+        }
+    }
+    return counted;
+}
+
+function countBs(string) {
+    return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
