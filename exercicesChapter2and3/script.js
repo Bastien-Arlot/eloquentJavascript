63390222298f5b159012e20df119f1395a62bb17
Bastien Arlot <arlot.b@gmail.com>
2022-04-19 15:49:43 +0200

    first part bean counting

--- exercicesChapter2and3/script.js
+++ exercicesChapter2and3/script.js
@@ -22,3 +22,11 @@ const isEven = (num) => {
 console.log(isEven(75));
 console.log(isEven(50));
 console.log(isEven(-9));
+
+//Bean Counting
+
+const countBs = (string) => {
+    return string.split("").filter(x => x === "B").length;
+}
+
+console.log(countBs("Bonjour les amis BBBBBb"));
