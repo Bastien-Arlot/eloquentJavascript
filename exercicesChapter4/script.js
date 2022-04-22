89baf0b76ea57d7bb669fbe91629f7e1a55032ae
Bastien Arlot <arlot.b@gmail.com>
2022-04-22 12:15:25 +0200

    add sum v2

--- exercicesChapter4/script.js
+++ exercicesChapter4/script.js
@@ -8,9 +8,18 @@ const range = (start, end) => {
     return arr;
 }
 console.log(range(1, 10));
-
+//Sum array
 const sum = (arr) => {
     return arr.reduce((a,b) => a + b, 0);
 
 }
 console.log(sum(range(1,10)));
+//Sum array v2
+const sum2 = (arr) => {
+    let sum = 0;
+    for (let elem of arr){
+        sum += elem;
+    }
+    return sum;
+}
+console.log(sum2(range(1,10)));
