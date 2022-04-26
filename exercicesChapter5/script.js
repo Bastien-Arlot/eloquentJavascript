7e7045f1b58d58c535e5bba968f55d017b0aadcc
Bastien Arlot <arlot.b@gmail.com>
2022-04-26 11:22:30 +0200

    add own loop exercice

--- exercicesChapter5/script.js
+++ exercicesChapter5/script.js
@@ -1,6 +1,16 @@
+//Flattening exercice
 const arr1 = [1,2,[1,2]];
 
 console.log(arr1.reduce((a,b) => a.concat(b), []));
-console.log(arrays.reduce((flat, current) => flat.concat(current), [])); // solution from the book
+console.log(arr1.reduce((flat, current) => flat.concat(current), [])); // solution from the book
 
+//Your Own Loop exercice
+
+const yourOwnLoop = (value, testFunction, updateFunction, bodyFunction ) => {
+    for (let i  = value; testFunction(i); i = updateFunction(i)){
+        bodyFunction(i);
+    }
+}
+
+yourOwnLoop(3, x => x > 0, x => x -1, console.log);
 
