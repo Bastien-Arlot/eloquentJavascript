917062ba65d314b12c9faed3c4b121404b97dbb8
Bastien Arlot <arlot.b@gmail.com>
2022-04-26 11:49:10 +0200

    add everything exercise

--- exercicesChapter5/script.js
+++ exercicesChapter5/script.js
@@ -1,10 +1,10 @@
-//Flattening exercice
+//Flattening exercise
 const arr1 = [1,2,[1,2]];
 
 console.log(arr1.reduce((a,b) => a.concat(b), []));
 console.log(arr1.reduce((flat, current) => flat.concat(current), [])); // solution from the book
 
-//Your Own Loop exercice
+//Your Own Loop exercise
 
 const yourOwnLoop = (value, testFunction, updateFunction, bodyFunction ) => {
     for (let i  = value; testFunction(i); i = updateFunction(i)){
@@ -14,3 +14,24 @@ const yourOwnLoop = (value, testFunction, updateFunction, bodyFunction ) => {
 
 yourOwnLoop(3, x => x > 0, x => x -1, console.log);
 
+//solution from the book
+function loop(start, test, update, body) {
+    for (let value = start; test(value); value = update(value)) {
+        body(value);
+    }
+}
+
+//Everything exercise
+
+const every = (arr, functionTest) => {
+    for(let i = 0; i < arr.length; i++){
+        if(!functionTest(arr[i])) return false;
+    }
+    return true;
+}
+const every2 = (arr, functionTest) => {
+    return !arr.some(x => !functionTest(x));
+}
+console.log(every([3,5,6], x => x > 100));
+
+console.log(every2([3,5,6], x => x < 100));
