901e7edee1a93fe799e8a3176ceed14b5e8be060
Bastien Arlot <arlot.b@gmail.com>
2022-04-26 10:21:12 +0200

    final exercice

--- exercicesChapter4/script.js
+++ exercicesChapter4/script.js
@@ -166,4 +166,21 @@ function nth(list, n) {
     if (!list) return undefined;
     else if (n == 0) return list.value;
     else return nth(list.rest, n - 1);
+};
+
+function deepEqual(a, b) {
+    if (a === b) return true;
+
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") return false;
+
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+
+    if (keysA.length != keysB.length) return false;
+
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
 }
