3de9e926902b9ae6dac65b8f3b9ff7d5db1732f7
Bastien Arlot <arlot.b@gmail.com>
2022-04-26 11:03:38 +0200

    flattening array

--- exercicesChapter5/script.js
+++ exercicesChapter5/script.js
@@ -1,5 +1,6 @@
 const arr1 = [1,2,[1,2]];
 
 console.log(arr1.reduce((a,b) => a.concat(b), []));
+console.log(arrays.reduce((flat, current) => flat.concat(current), [])); // solution from the book
 
 
