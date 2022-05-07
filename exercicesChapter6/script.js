bda4aae2a004bedfa526969ff2bed2554ef96636
Bastien Arlot <arlot.b@gmail.com>
2022-05-07 13:04:40 +0200

    first try ex1

--- exercicesChapter6/script.js
+++ exercicesChapter6/script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+//A Vector Type
+
+class Vec{
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(z){
+        return z + (this.x + this.y);
+    }
+    minus(z){
+        return z - (this.x + this.y);
+    }
+    get length(){
+        return console.log(this.x - 0, this.y - 0);
+    }
+}
+
+let Vec2 = new Vec(4,2);
+
+Vec2.length;
+console.log(Vec2.plus(2));
