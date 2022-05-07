4380b397f560dfcf54e74e5c182c97b118e04e1c
Bastien Arlot <arlot.b@gmail.com>
2022-05-07 13:40:34 +0200

    wip group ex

--- exercicesChapter6/script.js
+++ exercicesChapter6/script.js
@@ -1,29 +1,66 @@
 //A Vector Type
 
-class Vec{
-    constructor(x,y) {
+class Vec {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-    plus(newVec){
+
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    plus(newVec) {
         return new Vec(newVec.x + this.x, newVec.y + this.y);
     }
-    minus(newVec){
+
+    minus(newVec) {
         return new Vec(newVec.x - this.x, newVec.y - this.y);
-    }
-    get length(){
-        return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 }
 
-let vec2 = new Vec(3,4);
+let vec2 = new Vec(3, 4);
 
 console.log(vec2.length);
-console.log(vec2.plus(new Vec(3,3)));
+console.log(vec2.plus(new Vec(3, 3)));
 
-let vec3 = vec2.plus(new Vec(4,2));
+let vec3 = vec2.plus(new Vec(4, 2));
 console.log(vec3);
 
 console.log(vec3.length);
 
 console.log(new Vec(3, 4).length)
+
+//Groups
+
+class Group {
+    constructor() {
+        this.container = [];
+    }
+
+    add(value) {
+        if (!this.container.includes(value)) {
+            this.container.push(value);
+        }else{
+            alert("This value is already in the group");
+        }
+    }
+    remove(value){
+        if(this.container.includes(value)){
+          let index = this.container.indexOf(value);
+          this.container.splice(index,1);
+                }else{
+            alert("this value isnt in the group");
+        }
+            }
+        }
+
+let group1 = new Group();
+group1.add(2);
+console.log(group1);
+group1.add(5);
+console.log(group1);
+group1.remove(3);
+group1.add(5);
+group1.remove(2);
+console.log(group1);
